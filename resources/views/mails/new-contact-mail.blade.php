7a2ef8d0c51b128e690c98f4023f7c2a8fac038e
giuliagrfo <giuliagrifo1@gmail.com>
2023-01-25 16:39:01 +0100

    Add Email

---
+++ resources/views/mails/new-contact-mail.blade.php
@@ -0,0 +1,10 @@
+<h1>Ciao Admin!</h1>
+
+<p>
+<h3>Hai un nuovo messaggio!</h3>
+
+<span>Nome: {{ $lead->name }}</span> <br>
+<span>Mail: {{ $lead->email }}</span> <br>
+<span>Messaggio: {{ $lead->message }}</span>
+
+</p>
